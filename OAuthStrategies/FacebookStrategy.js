694aab1e5136f4053fd9ee4ac4230baf136e6349
Eli Weber <eliweber2001@gmail.com>
2022-08-27 15:42:22 -0400

    Callbacks changed

--- OAuthStrategies/FacebookStrategy.js
+++ OAuthStrategies/FacebookStrategy.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    callbackURL: "https://jwt-oauth-app.herokuapp.com/auth/facebook/callback",
     profileFields: ['id', 'displayName', 'photos', 'email']
   },
   function(accessToken, refreshToken, profile, cb) {
